ecceb11cbf2d5151e821ce163b676715a38a248c
dcoulston@hotmail.co.uk <dcoulston@hotmail.co.uk>
2022-02-06 14:18:17 +0000

    minor updates

--- DFESW-7-Final/src/main/java/com/qa/games_app/exceptions/GameNotFoundException.java
+++ DFESW-7-Final/src/main/java/com/qa/games_app/exceptions/GameNotFoundException.java
@@ -8,9 +8,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 public class GameNotFoundException extends EntityNotFoundException {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	public GameNotFoundException(String message) {
